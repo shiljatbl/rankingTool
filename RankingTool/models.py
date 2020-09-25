3a9512b84476fd30a184b66077596fa620ba32cd
Goran Ljiljak <goran.ljiljak@bridgewaterlabs.com>
2020-09-25 16:14:34 +0200

    Added test product

--- RankingTool/models.py
+++ RankingTool/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Product(models.Model):
+    asin = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.asin
+
+
 
