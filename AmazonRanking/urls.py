421fc270677a106178de3902abb9e62d5cc67d28
Goran Ljiljak <goran.ljiljak@bridgewaterlabs.com>
2020-09-25 13:54:14 +0200

    Added views and static files

--- AmazonRanking/urls.py
+++ AmazonRanking/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from pages import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('', views.home_view, name="home"),
     path('admin/', admin.site.urls),
+    
 ]
+
+urlpatterns += staticfiles_urlpatterns()
