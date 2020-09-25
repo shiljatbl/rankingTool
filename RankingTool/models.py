f2752d7384ed838c32142063024f5ec47563633f
Goran Ljiljak <goran.ljiljak@bridgewaterlabs.com>
2020-09-25 17:20:43 +0200

    Added product to admin dashboard

--- RankingTool/models.py
+++ RankingTool/models.py
@@ -3,7 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    asin = models.CharField(max_length=10)
+    asin = models.CharField(max_length=10, default="Test")
+    title = models.CharField(max_length=200,default="Test")
+    price = models.FloatField(default=0.0)
+    position = models.IntegerField(default=0)
+    page = models.IntegerField(default=0)
+
+
+
 
     def __str__(self):
         return self.asin
